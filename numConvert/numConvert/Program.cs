a608f8d36730e878a923310b8b171a01d4e6de91
Joshua Petersen <jpetersen@my365-uwc.ac.za>
2020-11-19 12:02:25 +0200

    new changes

--- numConvert/numConvert/Program.cs
+++ numConvert/numConvert/Program.cs
@@ -2,11 +2,45 @@
 
 namespace numConvert
 {
-    class Program
+     class Program
     {
-        static void Main(string[] args)
+        public static string NumberToWords(int number)
         {
-            Console.WriteLine("Hello World!");
+            if (number == 0) return "zero";
+            if (number < 0) return "minus" + NumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + "thousand";
+                 number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + "hundred";
+                number %= 100;
+            }
+
+            if ((number / 100) > 0)
+            {
+                if (words != "") words += "and";
+
+                var unitsMap = new[] {"Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+                              "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+                var tensMap = new[] { "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninty" };
+                if (number < 20) words += unitsMap[number];
+                else
+                {
+                    words += tensMap[number / 10];
+
+                    if ((number % 10) > 0) words += "-" + unitsMap[number % 10];
+                }
+
+            }
+            return words;
         }
+        
     }
 }
